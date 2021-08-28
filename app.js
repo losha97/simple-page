2de884b7aa63ca67219f369559c808947830125e
Aleksiej Kuncewicz <aleksiej.kuncewicz@gmail.com>
2021-08-28 14:14:14 +0200

    create index.html with styles.css; connect app.js for displaying current year via id; add 2 headings (1 in header, other in main), 1 paragraph, nav with ul li itesms of links, section, and footer; add favicon with covered license.

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+* @Author: Aleksiej Kuncewicz
+* @Date:   2021-08-28 14:04:19
+* @Last Modified by:   Aleksiej Kuncewicz
+* @Last Modified time: 2021-08-28 14:13:14
+*/
+
+const date = new Date().getFullYear();
+document.getElementById("year").innerHTML = date;
